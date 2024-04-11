1af883ed7d461bb47a5d112dabd9a4c1dcc3f4b3
adhikari-amit <adhikariamit2001@gmail.com>
2024-04-11 14:17:28 +0530

    Html template render 🪁
    01. Used GO std library template() to parse the html files

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"text/template"
+
 	// Std library to handel http request on Go
 	"net/http"
 )
@@ -10,12 +12,25 @@ const PORT = ":8080"
 
 // Home Route handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home page.")
+	renderTemplate(w, "home.html")
 }
 
 // About Route handler
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About page.")
+	renderTemplate(w, "about.html")
+
+}
+
+// Function to Parse the html files
+func renderTemplate(w http.ResponseWriter, temp string) {
+	// Go Std. library template used to parse the html file
+	parsedTemp, _ := template.ParseFiles("./templates/" + temp)
+	err := parsedTemp.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parshing the template", err)
+		return
+	}
+
 }
 
 func main() {
