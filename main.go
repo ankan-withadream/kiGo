15dbb583d1dad62b718182422eb903ec73bcc2c1
ama3it <adhikariamit2001@gmail.com>
2024-04-16 15:55:23 +0530

    GO fiber implement /kigo route

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
-	// Std library to handel http request on Go
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
 	"github.com/gofiber/fiber/v3"
 )
 
 const PORT = ":8080"
 
+type Response struct {
+}
+
 func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/kigo", func(c fiber.Ctx) error {
+
+		response, err := http.Get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=43272343095c48fb98d7463190d2cf2f")
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		bodyBytes, err := io.ReadAll(response.Body)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// return c.SendString("Hello, World!")
+		return c.Send(bodyBytes)
 	})
 
 	// http request listener
