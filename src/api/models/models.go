2d6e46daeb8ffe5e3e197402c3fd57c8df2d7fdb
Ankan <ankanwithadream@gmail.com>
2024-06-25 15:18:52 +0530

    Update Chatroom model: added sender receiver name

--- src/api/models/models.go
+++ src/api/models/models.go
@@ -3,7 +3,9 @@ package models
 type Chatroom struct {
 	ID              uint `gorm:"primaryKey"`
 	sender_id       int
+	sender_name 	*string
 	receiver_id     int
+	receiver_name 	*string
 	messages_recent *string
 	messages_backup *string
 }
